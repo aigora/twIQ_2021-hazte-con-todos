0d2d8155863967703f75104bb75ca3f2384c67ea
Constan-14 <80398653+Constan-14@users.noreply.github.com>
2021-04-23 13:12:56 +0200

    Update Hazte con todos.c

--- Hazte con todos.c
+++ Hazte con todos.c
@@ -2,129 +2,154 @@
 #include <string.h>
 #define N 100
 
+struct buscador{
+	char nombre[30];
+	int npokedex;
+	int generacion;
+	char tipo[100];
+	char debilidad[100];
+	char evoluciones[100];
+	};
 struct equipo {
-	char pokemon1[N];
-	char pokemon2[N];
-	char pokemon3[N];
-	int nPotis;
-};	
-int main (){
-  struct equipo pokemons[N];
-  int npokedex;
-  int seleccion,volver,i;
-  char entrenador[N];
+	char pokemon[N];
+
+};
+int main () {
+	FILE*fichero;
+	char modoBusqueda;
+	struct buscador poke[6] = { {"Bulbasaur",1,1,"Planta-Veneno","Fuego, Psiquico, Volador y Hielo","Bulbasaur-Ivysaur-Venusaur"},
+								{"Ivysaur",2,1,"Planta-Veneno","Fuego, Psiquico, Volador y Hielo","Bulbasaur-Ivysaur-Venusaur"},
+								{"Venusaur",3,1,"Planta-Veneno","Fuego, Psiquico, Volador y Hielo","Bulbasaur-Ivysaur-Venusaur"},
+								{"Charmander",4,1,"Fuego","Agua, Tierra y Roca","Charmander-Charmeleon-Charizard"},
+								{ "Charmeleon",5,1,"Fuego","Agua, Tierra y Roca","Charmander-Charmeleon-Charizard"},
+								{"Charizard",6,1,"Fuego","Agua, Tierra y Roca","Charmander-Charmeleon-Charizard"}};
+	struct equipo pokemons[3];
+	int seleccion,volver,i,M,p;
+	char entrenador[N],nombre[100];
+	int nPokedex,m;
+
+	/*fichero = fopen("Hazte con todos.txt","r")
+	if(fichero == NULL){
+		printf("No existe el archivo");
+		return 0;
+	}
+	i = 0;
+	while(fscanf(fichero, "%s %f",pokemons[i].pokemon) != EOF){
+		i++;
+	}*/
+
+
+
 
 	printf("\t---------------------------------\n");
 	printf("\t|*Bienvenido a'HAZTE CON TODOS'*|\n");
 	printf("\t---------------------------------\n");
-	
-  do{
-	printf("\t|Pulse 1 para entrar al menu de la Pokedex \n");
-  	printf("\t|Pulse 2 para entrar al creador de equipos\n");
-	printf("\t|Pulse 3 para salir\n");
-	printf("\t|Pulse 4 para saber cosas de los creadores\n");
-	printf("\t-------------------------------------\n");
-	scanf("%d", &seleccion);
-		
-		switch(seleccion){
+
+	do {
+		printf("\t|Pulse 1 para entrar al menu de la Pokedex \n");
+		printf("\t|Pulse 2 para entrar al creador de equipos\n");
+		printf("\t|Pulse 3 para salir\n");
+		printf("\t|Pulse 4 para saber cosas de los creadores\n");
+		printf("\t-------------------------------------\n");
+		scanf("%d", &seleccion);
+
+		switch(seleccion) {
 			case 1:
-				volver=0;
-				
+				volver = 0;
+
 				printf("\tIntroduce tu nombre de entrenador:\n");
 				scanf("%s", entrenador);
 				printf("\tBienvenido al menu %s, aqui podras buscar informacion sobre el pokemon que quieras\n",entrenador);
-				printf("\tAhora introduzca el nonmbre del pokemon que quieres ver\n"); //De aqui mandara al nombre de la pokedex a la que corresponde el pokemon, en el cual se mostrara toda la información
-				scanf("%d", &npokedex);
-			break;
-			
+
+				break;
+
 			case 2:
-				volver=0;
+				volver = 0;
 				printf("\t Bienvenido entrenador, has entrado en el creador de equipos, aqui podras seleccionar los pokemons que habra en tu equipo asi como los objetos que usaras en tus peleas\n");
-				printf("\tAhora indica el nombre de tu primer pokemon\n");
-				scanf("%s",&pokemons[i].pokemon1);
-				printf("\tAhora indica el nombre de tu segundo pokemon\n");
-				scanf("%s",&pokemons[i].pokemon2);
-				printf("\tAhora indica el nombre de tu tercer pokemon\n");
-				scanf("%s",&pokemons[i].pokemon3);
-				printf("Por ultimo indica el numero de pociones de curacion que quieres emplear\n");
-				scanf("%d", &pokemons[i].nPotis);
-			
+				//fentrada=fopen("Equipo.txt", "w");
+				printf("\tEscribe el numero de pokemons que habra en el equipo\n");
+				scanf("%d", &M);
+				for(i = 0 ; i < M ; i++){
+					printf("\tEscribe el nombre del pokemon %d\n", i+1);
+					scanf("%s", &pokemons[i].pokemon);
+				}
+				return 0;
+
 			case 3:
-				printf("\t!!!Hasta la proxima!!!\n");	
-			return 0;
-			
+				printf("\t!!!Hasta la proxima!!!\n");
+				return 0;
+
 			case 4:
-				volver=0;
+				volver = 0;
 				
-				printf("\tLos creadores de este proyecto somos:\n");
-				printf("\tConstantino Janeiro Catena\n");
-				printf("\tAdrian Langarica Esteban\n");
-				printf("\tRuben Ernesto Godoy Salcedo\n");                                                            
+				printf("\t========================================\n");
+				printf("\t|Los creadores de este proyecto somos: |\n");
+				printf("\t------*Constantino Janeiro Catena*------\n");
+				printf("\t-------*Adrian Langarica Esteban*-------\n");
+				printf("\t------*Ruben Ernesto Godoy Salcedo*------\n");
+				printf("\t========================================\n");
 				printf("\tPulse 1 si desea volver a la pantalla de inicio o 2 si quiere salir\n");
 				scanf("%d", &seleccion);
-				
-				switch(seleccion){
+
+				switch(seleccion) {
 					case 1:
 						printf("\tHas elegido volver al menu\n");
 						volver++;
-					break;
+						break;
 					case 2:
 						printf("\t!!!Hasta la proxima!!");
-					return 0;
+						return 0;
 				}
+				break;
+		}	
+	} while(volver == 1);
+	
+	
+	printf("\tIndica como quiere buscar el pokemon, si desea buscarlo por el numero de la pokedex escriba N, si lo prefiere por nombre escriba M\n");
+	scanf("%s", &modoBusqueda);
+	
+	if(modoBusqueda == 'N'|| modoBusqueda == 'n'){
+		p=1;}
+	else{
+		p=2;}
+	
+	switch(p){
+		case 1:
+			printf("\tHa selecionado buscar por el numero de la pokedex\n");
+			printf("\tIntroduce el numero del pokemon\n");
+			scanf("%d", &nPokedex);
 			break;
-		}
+		case 2:
+			printf("\tHa selecionado buscar por el nombre del pokemon\n");
+			
+			break;
 	}
-	while(volver==1);
-    
-return 0;
-}
-    printf("\t-----------\n");
-	printf("\t|Bulbasaur|\n");
-	printf("\t-----------\n");
-    printf("\tGeneracion: 1\n");
-	printf("\tTipo: Planta y Veneno \n");
-	printf("\tDebilidad: Fuego, Psiquico, Volador y Hielo\n");
-	printf("\tEvoluciones: Bulbasaur-Ivysaur-Venusaur\n");
+	
+	while(1)
+	{	
+		printf("\tEscriba el nombre del pokemon\n");
+		scanf("%s",nombre);
+		for(i = 0 ; i < 6; i++){
+			if(nombre[0] == poke[i].nombre[0]){ 
+	
+				printf("\t*Pokemon*\n");
+				printf("\t-----------\n");
+				printf("\t|%s|\n",poke[i].nombre);
+				printf("\tGeneracion: %d\n",poke[i].generacion);
+				printf("\tNumero de Pokedex: %d\n", poke[i].npokedex);
+				printf("\tTipo: %s\n",poke[i].tipo);
+				printf("\tDebilidad: %s\n",poke[i].debilidad);
+				printf("\tEvoluciones: %s\n",poke[i].evoluciones);
+				
+				m++;
+			}
+		}
 		
-	printf("\t---------\n");
-	printf("\t|Ivysaur|\n");
-	printf("\t---------\n");
-	printf("\tGeneracion: 1\n");
-	printf("\tTipo: Planta y Veneno \n");
-	printf("\tDebilidad: Fuego, Psiquico, Volador y Hielo\n");
-	printf("\tEvoluciones: Bulbasaur-Ivysaur-Venusaur\n");
+		if(m == 0)
+			printf("Pokemon no encontrado\n");
+	}
 
-	
-    printf("\t----------\n");
-	printf("\t|Venusaur|\n");
-	printf("\t----------\n");
-	printf("\tGeneracion: 1\n");
-	printf("\tTipo: Planta y Veneno \n");
-	printf("\tDebilidad: Fuego, Psiquico, Volador y Hielo\n");
-	printf("\tEvoluciones: Bulbasaur-Ivysaur-Venusaur\n");
-	
-	printf("\t------------\n");
-	printf("\t|Charmander|\n");
-	printf("\t------------\n");
-	printf("\tGeneracion: 1\n");
-	printf("\tTipo: Fuego \n");
-	printf("\tDebilidad: Agua, Tierra y Roca\n");
-	printf("\tEvolucion: Charmander-Charmeleon-Charizard\n");
-	
-	printf("\t------------\n");
-	printf("\t|Charmeleon|\n");
-	printf("\t------------\n");
-	printf("\tGeneracion: 1\n");
-	printf("\tTipo: Fuego \n");
-	printf("\tDebilidad: Agua, Tierra y Roca\n");
-	printf("\tEvolucion: Charmander-Charmeleon-Charizard\n");
-	
-	printf("\t-----------\n");
-	printf("\t|Charizard|\n");
-	printf("\t-----------\n");
-	printf("\tGeneracion: 1\n");
-	printf("\tTipo: Fuego y Volador\n");
-	printf("\tDebilidad: Agua, Tierra y Roca\n");
-	printf("\tEvolucion: Charmander-Charmeleon-Charizard\n");
-	
+	return 0;
+}
+
+
