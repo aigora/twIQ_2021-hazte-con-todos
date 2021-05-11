e947a4bf93d9e6c66c9fb4cc3c101f8045d40543
adrianlan79 <80395169+adrianlan79@users.noreply.github.com>
2021-05-11 13:55:54 +0200

    Revert "Update Hazte con todos.c"
    This reverts commit 7abe54066891144ea25dbc20afde525ee2fca60a.

--- Hazte con todos.c
+++ Hazte con todos.c
@@ -446,11 +446,8 @@ int main () {
 						printf("\t---------------------------------------\n");
 					}
 				}
-				printf("\n");
-			printf("\t---------------------------------------\n");
-			printf("\t|¿Desean consultar algún otro pokemon?|\n");
-			printf("\t|Escriba S o N                        |\n");
-			printf("\t---------------------------------------\n");
+				printf("\t¿Desean consultar algun otro pokemon?\n");
+				printf("\tEscriba S o N\n");
 				scanf("%s", &opcion1);
 
 				switch(opcion1){
@@ -489,11 +486,8 @@ int main () {
 				}
 			}while(m == 0);
 			
-			printf("\n");
-			printf("\t---------------------------------------\n");
-			printf("\t|¿Desean consultar algún otro pokemon?|\n");
-			printf("\t|Escriba S o N                        |\n");
-			printf("\t---------------------------------------\n");
+			printf("\t¿Desean consultar algun otro pokemon?\n");
+				printf("\tEscriba S o N\n");
 				scanf("%s", &opcion1);
 				
 				switch(opcion1){
